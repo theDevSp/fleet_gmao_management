bd84b4f3cef5280ec2efc63216eb08edbbbde10d
theDevOps <62214243+theDevSp@users.noreply.github.com>
2023-05-25 16:07:35 +0100

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'account_fiscal_year_period', 'fleet','construction_site_management'],
+    'depends': ['base', 'account_fiscal_year_period', 'fleet','construction_site_management','sale', 'purchase', 'stock',],
 
     # always loaded
     'data': [
