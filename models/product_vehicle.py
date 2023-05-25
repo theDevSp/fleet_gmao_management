53de7a279018e6d148dce6a075a0daf2ac9f1387
theDevOps <62214243+theDevSp@users.noreply.github.com>
2023-05-25 15:21:33 +0100

    Update product_vehicle.py

--- models/product_vehicle.py
+++ models/product_vehicle.py
@@ -9,14 +9,7 @@ class product_template_type(models.Model):
 
 	name = fields.Char('Nom Type', size=64)
 
-class product_nomenclature(models.Model):
-	_name = "product.nomenclature"
 
-	name = fields.Char("Modèle", required=True)
-	
-	_sql_constraints = [
-		('name_uniq', 'unique(name)', 'Le nom du modèle doit être unique.'),
-	]
 
 class product_brand(models.Model):
 	_name = "product.brand"
